aadf3d59fa53dd844218d0fb8d2a9bc4cbeb9211
oliversh99 <oliv.code999@gmail.com>
2021-07-24 11:46:03 -0700

    add 4th java file

--- SingleAnimal.js
+++ SingleAnimal.js
@@ -160,23 +160,3 @@ function createTable(animalInfoArray){
 
 
     }) 
-// call by id 
-//     $( document ).ready(function() {
-// getParams()
-
-//     })
-// function getParams(){
-//     var searchParam = document.location.search.split('&');
-
-//     var id = 
-//     getanimalDetales (id)
-// }
-
-// function getanimalDetales(id){
-
-
-// fetch(' https://api.petfinder.com/v2/animalstype')
-
-
-    // inside click action will call open weather search and hand it the 5th index of the array
-
